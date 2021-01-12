fcb50bfdfd1ceab34094230d91ba123867aee409
mqt <mqt@MacBook-Pro.local>
2021-01-12 14:36:29 +1100

    fixed proxy middleware config

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/welcome', { target: 'http://localhost:5000' }));
- 
+  app.use(
+    '/welcome',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
 };
